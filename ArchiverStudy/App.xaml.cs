165f9685dedf1fae30c534689ad180dce7aa051c
YuhSylphy <yuhsylphy@gmail.com>
2021-02-04 22:04:09 +0900

    feat: Apply latest codes
    * shows context menu only
    * switch window with command line args

--- ArchiverStudy/App.xaml.cs
+++ ArchiverStudy/App.xaml.cs
@@ -5,15 +5,50 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using static ArchiverStudy.Common.MyExtensions;
 
 namespace ArchiverStudy
 {
+    using Views;
+    using CommandLine;
     public partial class App : Application
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             DispatcherHelper.UIDispatcher = Dispatcher;
+
+            Parser.Default.ParseArguments<MenuOptions, BrowseOptions>(e.Args)
+                .MapResult<MenuOptions, BrowseOptions, object?>(
+                (MenuOptions o) =>
+                {
+                    MainWindow = new MainMenu();
+                    return null;
+                }, 
+                (BrowseOptions o) =>
+                {
+                    MainWindow = new MainWindow();
+                    MainWindow.Show();
+                    return null;
+                },
+                err =>
+                {
+                    return null;
+                });
             //AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        [Verb("menu", isDefault: true, HelpText = "shows root context menu")]
+        class MenuOptions
+        {
+            [Value(0, HelpText = "specify target paths", MetaName = "target paths", Required = true)]
+            public IEnumerable<string> TargetPaths { get; set; } = Array.Empty<string>();
+        }
+
+        [Verb("browse", HelpText = "shows archive browser")]
+        class BrowseOptions
+        {
+            [Value(0, HelpText = "specify target paths", MetaName = "target paths")]
+            public string? TargetPath { get; set; }
         }
 
         // Application level error handling
---
+++ ArchiverStudy/Behaviors/HiddenWindowBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ArchiverStudy.Behaviors
+{
+    internal class HiddenWindowBehavior
+    {
+        public static readonly DependencyProperty IsHiddenProperty = DependencyProperty.RegisterAttached(
+            "IsHidden",
+            typeof(bool),
+            typeof(HiddenWindowBehavior),
+            new PropertyMetadata(false, OnIsHiddenChanged)
+            );
+
+        public static bool GetIsHidden(DependencyObject target) => (bool)target.GetValue(IsHiddenProperty);
+        public static void SetIsHidden(DependencyObject target, bool value) => target.SetValue(IsHiddenProperty, value);
+
+        private static void OnIsHiddenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Window element && e.NewValue is bool value && value)
+            {
+                element.Hide();
+                element.Initialized += OnInitialized;
+            }
+        }
+
+        private static void OnInitialized(object? sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("OnLoad");
+            if (sender is Window element)
+            {
+                element.ContextMenu.IsOpen = true;
+                element.ContextMenu.Closed += (sender, e) =>
+                {
+                    element.Close();
+                };
+            }
+        }
+    }
+}
